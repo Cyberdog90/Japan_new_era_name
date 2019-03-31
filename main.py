20de53799544065590f77e3c2697a9e1f5c32e07
Cyberdog90 <43635561+Cyberdog90@users.noreply.github.com>
2019-03-31 22:22:01 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 f = open("kanji.txt", encoding="utf-8")
 kanji_list = f.readline()
 f.close()
+f = open("era.txt", "wb")
 for i in kanji_list:
     for j in kanji_list:
         print("{}{}".format(i, j))
+        f.write("{}{}".format(i, j).encode())
+f.close()
